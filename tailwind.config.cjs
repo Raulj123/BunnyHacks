2268da51366a5eba940a0760fc609a55d74f0e24
raulj <112786129+Raulj123@users.noreply.github.com>
2024-03-31 12:06:30 -0700

    🎉 Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config}*/
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	daisyui: {
+		themes: ['cupcake'],
+	},
+	theme: {
+		extend: {}
+	},
+
+	plugins: [require('daisyui')]
+};
+
+module.exports = config;
